c3c74f04d3b0b142614bdd952c306a732119c041
Gabriel <72815196+okoro282@users.noreply.github.com>
2021-08-17 19:22:14 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './style.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [data, setData] = useState("")
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach(item => {
+    calcBtns.push(
+      <button
+        onClick={e => {
+          setData(data + e.target.value)
+        }}
+        value={item}
+        key={item}>
+        {item}
+      </button>
+
+    )
+  })
+  return (
+    <div className='wrapper'>
+      <div className="show-input">
+        {data}
+      </div>
+      <div className='digits flex'>{calcBtns} </div>
+      <div className="modifiers grid">
+        <button onClick={() => setData(data.substr(0, data.length - 1))}>
+          Clear
+        </button>
+        <button onClick={() => setData("")}>
+          Ac
+        </button>
+      </div>
+      <div className="operations subgrid">
+        <button onClick={e => setData(data + e.target.value)} value="+">
+          +
+        </button>
+        <button onClick={e => setData(data + e.target.value)} value="-">
+          -
+        </button>
+        <button onClick={e => setData(data + e.target.value)} value="*">
+          *
+        </button>
+        <button onClick={e => setData(data + e.target.value)} value="/">
+          /
+        </button>
+        <button onClick={e => {
+          try {
+            setData(
+              String(eval(data)).length > 3 &&
+                String(eval(data).includes)(".")
+                ? String(eval(data).toFixed(4))
+                : String(eval(data))
+            )
+          }
+          catch (err) {
+            console.log(err)
+          }
+        }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement);
